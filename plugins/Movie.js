47f7cf2b75b65cc9603b3609c851c4a24033b69c
Thisan Indusara <85940832+Thisan01@users.noreply.github.com>
2022-08-03 10:29:58 -0700

    Update Movie.js

--- plugins/Movie.js
+++ plugins/Movie.js
@@ -11,23 +11,23 @@ module.exports = {
         if(response.Response=== 'False') return msg.reply('Movie not found!')
         
         let str = '';
-        str += 'Title      : ' + response.Title + '\n\n';
-        str += 'Year       : ' + response.Year + '\n\n';
-        str += 'Rated      : ' + response.Rated + '\n\n';
-        str += 'Released   : ' + response.Released + '\n\n';
-        str += 'Runtime    : ' + response.Runtime + '\n\n';
-        str += 'Genre      : ' + response.Genre + '\n\n';
-        str += 'Director   : ' + response.Director + '\n\n';
-        str += 'Writer     : ' + response.Writer + '\n\n';
-        str += 'Actors     : ' + response.Actors + '\n\n';
-        str += 'Plot       : ' + response.Plot + '\n\n';
-        str += 'Language   : ' + response.Language + '\n\n';
-        str += 'Country    : ' + response.Country + '\n\n';
-        str += 'Awards     : ' + response.Awards + '\n\n';
-        str += 'BoxOffice  : ' + response.BoxOffice + '\n\n';
-        str += 'Production : ' + response.Production + '\n\n';
-        str += 'imdbRating : ' + response.imdbRating + '\n\n';
-        str += 'imdbVotes  : ' + response.imdbVotes;
+        str += '(kyloren) Title      : ' + response.Title + '\n\n';
+        str += '(kyloren) Year       : ' + response.Year + '\n\n';
+        str += '(kyloren) Rated      : ' + response.Rated + '\n\n';
+        str += '(kyloren) Released   : ' + response.Released + '\n\n';
+        str += '(kyloren) Runtime    : ' + response.Runtime + '\n\n';
+        str += '(kyloren) Genre      : ' + response.Genre + '\n\n';
+        str += '(kyloren) Director   : ' + response.Director + '\n\n';
+        str += '(kyloren) Writer     : ' + response.Writer + '\n\n';
+        str += '(kyloren) Actors     : ' + response.Actors + '\n\n';
+        str += '(kyloren) Plot       : ' + response.Plot + '\n\n';
+        str += '(kyloren) Language   : ' + response.Language + '\n\n';
+        str += '(kyloren) Country    : ' + response.Country + '\n\n';
+        str += '(kyloren) Awards     : ' + response.Awards + '\n\n';
+        str += '(kyloren) BoxOffice  : ' + response.BoxOffice + '\n\n';
+        str += '(kyloren) Production : ' + response.Production + '\n\n';
+        str += '(kyloren) imdbRating : ' + response.imdbRating + '\n\n';
+        str += '(kyloren) imdbVotes  : ' + response.imdbVotes;
         await conn.sendMessage(msg.from, {image :{url:response.Poster},caption: str});
 	}
 };
