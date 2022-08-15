426e88a67fcc591cad36e096891a59bea68f4b07
Thisan Indusara <85940832+Thisan01@users.noreply.github.com>
2022-08-16 01:23:29 +0530

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -51,7 +51,7 @@ module.exports = {
       commits["all"].map((commit, num) => {
         availupdate += num + 1 + " ●  " + tiny(commit.message) + "\n";
       });
-      await conn.sendMessage(msg.from, {
+      return await conn.sendMessage(msg.from, {
         text: availupdate,
         footer: tiny("click here to update"),
         buttons: [
